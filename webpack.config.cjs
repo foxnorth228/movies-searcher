0e5b7ad5e849109aa73d6ca2808067df24ae39f4
Alex Khitry <khitrii03@gmail.com>
2023-08-31 21:19:21 +0300

    init: init repository

---
+++ webpack.config.cjs
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      "@src":        path.resolve( __dirname, "src"),
+      "@assets":     path.resolve( __dirname, "src/assets"),
+      "@components": path.resolve( __dirname, "src/components"),
+      "@constants":   path.resolve( __dirname, "src/constants"),
+      "@pages":      path.resolve( __dirname, "src/pages"),
+      "@store":   path.resolve( __dirname, "src/store"),
+      "@utils":   path.resolve( __dirname, "src/utils"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/assets',
+          to: '',
+          noErrorOnMissing: true
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './public'),
+    }
+  }
+}
