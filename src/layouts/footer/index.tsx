de4a6173d6d2e21395863177b82f707ff4dc3fb1
Alex Khitry <khitrii03@gmail.com>
2023-09-01 23:22:04 +0300

    feat: implement footer view

--- src/layouts/footer/index.tsx
+++ src/layouts/footer/index.tsx
@@ -2,7 +2,29 @@ import React from 'react';
 import './style.css';
 
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer className="footer">
+      <section className="footer__container">
+        <article className="footer__textBlock_first">
+          <p>TermsPrivacyPolicy & Safety</p>
+          <p>How YouTube works</p>
+          <p>Test new features</p>
+        </article>
+        <article className="footer__textBlock_second">
+          <p>About Press Copyright</p>
+          <p>Contact us Creators</p>
+          <p>Advertise Developers</p>
+        </article>
+        <article className="footer__icons">
+          <i>icon</i>
+          <i>icon</i>
+          <i>icon</i>
+          <i>icon</i>
+        </article>
+      </section>
+      <h2 className='footer__title'>2023 Modsen company</h2>
+    </footer>
+  );
 };
 
 export default Footer;
