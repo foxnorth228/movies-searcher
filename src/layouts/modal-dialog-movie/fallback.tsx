eabf0bf559788e169b59ea4001f48ecb745d5c4b
Alex Khitry <khitrii03@gmail.com>
2023-09-09 23:14:59 +0300

    feat: implement modalDialogMovie layout

---
+++ src/layouts/modal-dialog-movie/fallback.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieFallback = () => {
+  return (
+    <article className="movie__fallback">
+      <div className="movie__fallback_image skeleton"></div>
+    </article>
+  );
+};
+
+export default MovieFallback;
--- src/layouts/modal-dialog-movie/index.tsx
+++ src/layouts/modal-dialog-movie/index.tsx
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { useSelectedMovie } from '@store/moviesSlice';
+import MovieFallback from '@src/layouts/modal-dialog-movie/fallback';
 
 const ModalDialogMovie = () => {
   const [selectedMovie, setSelectedMovie] = useSelectedMovie();
+  const [isLoadedFrame, setIsLoadedFrame] = useState(false);
+
   let trailer;
   if ('trailer' in selectedMovie) {
     trailer = selectedMovie['trailer'];
   }
+  const notNullTrailer = typeof trailer === 'object' && trailer;
+  const movie = notNullTrailer && 'linkEmbed' in notNullTrailer && notNullTrailer['linkEmbed'];
+
   const urlParams = new URLSearchParams();
   urlParams.append('autoplay', 'false');
   urlParams.append('width', '640');
   return (
     <section onClick={() => setSelectedMovie('')} className="modalDialogMovie">
-      <iframe
-        className="modalDialogMovie__video"
-        src={String(
-          trailer && typeof trailer === 'object' && 'linkEmbed' in trailer && trailer['linkEmbed']
-        ) + '?' + urlParams.toString()}
-        width="640"
-        height="480"
-        allowFullScreen={true}
-        frameBorder="no"
-        scrolling="no"
-      ></iframe>
+      <article className="modalDialogMovie__content">
+        {!isLoadedFrame && typeof movie === 'string' && <MovieFallback />}
+        {typeof movie === 'string' ? (
+          <iframe
+            className="modalDialogMovie__video"
+            src={String(movie) + '?' + urlParams.toString()}
+            width="640"
+            height="480"
+            allowFullScreen={true}
+            frameBorder="no"
+            scrolling="no"
+            onLoad={() => setIsLoadedFrame(true)}
+          ></iframe>
+        ) : (
+          <h1 className="modalDialogMovie__errorMessage">
+            {'Video is not available: '}
+            {notNullTrailer &&
+              'errorMessage' in notNullTrailer &&
+              (notNullTrailer['errorMessage'] as string)}
+          </h1>
+        )}
+      </article>
     </section>
   );
 };
